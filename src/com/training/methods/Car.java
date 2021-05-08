0a3007665ede10dbe437f77432ce34f5ca4706a2
Janani Velmurugan <janani@hexaware.com>
2021-05-08 12:24:34 +0530

    added hashcode equals demo

--- src/com/training/methods/Car.java
+++ src/com/training/methods/Car.java
@@ -65,6 +65,35 @@ public class Car {
 		return price;
 	}
 
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((model == null) ? 0 : model.hashCode());
+		result = prime * result + speed;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		if (model == null) {
+			if (other.model != null)
+				return false;
+		} else if (!model.equals(other.model))
+			return false;
+		if (speed != other.speed)
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
 		return "Car [model=" + model + ", speed=" + speed + ", price=" + price + "]";
