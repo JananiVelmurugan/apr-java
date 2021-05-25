b9ba1466bf54c1581de28e92b06fc270c2541be9
Janani Velmurugan <janani@hexaware.com>
2021-05-25 10:08:01 +0530

    updated db connection url chainsys

--- src/com/training/exception/UnCheckedExceptionDemo.java
+++ src/com/training/exception/UnCheckedExceptionDemo.java
@@ -2,26 +2,32 @@ package com.training.exception;
 
 public class UnCheckedExceptionDemo {
 
-	public static void main(String[] args) {
-		try {
-			int result = 5 / 0;
-			System.out.println(result);
-		} catch (ArithmeticException ae) {
-			System.out.println(ae.getMessage());
-		}
+	public static void main(String[] args) throws NullPointerException {
+//		try {
+		// error prone code
+//			int result = 5 / 0;
+//			System.out.println(result);
+//		} catch (ArithmeticException ae) {
+//			 handler to the error prone code
+//			System.out.println(ae.getMessage());
+//			System.out.println(ae);
+//			ae.printStackTrace();
+//			System.out.println("Invalid Division");
+//		}
 
-		try {
-			int[] numbers = { 1, 2, 3 };
-			System.out.println(numbers[10]);
-		} catch (ArrayIndexOutOfBoundsException aioe) {
-			aioe.printStackTrace();
-		}
-		try {
-			String name = null;
-			System.out.println(name.length());
-		} catch (NullPointerException npe) {
-			System.out.println(npe);
-		}
+//		try {
+//			int[] numbers = { 1, 2, 3 };
+//			System.out.println(numbers[10]);
+//		} catch (ArrayIndexOutOfBoundsException aioe) {
+//			aioe.printStackTrace();
+//		}
+//		try {
+//			String name = null;
+//			int result = 5 / 0;
+//			System.out.println(name.length());//null.length()
+//		} catch (ArithmeticException npe) {
+//			System.out.println("Null Pointer Exception");
+//		}
 		try {
 			String user = "lion";
 			System.out.println(user.charAt(7));
